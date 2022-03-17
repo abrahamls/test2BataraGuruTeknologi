23d0d31e99eecf4e6a9e28ecca78f72a7c1a909d
abraham linggom siahaan <abraham.siahaan1997@gmail.com>
2022-03-17 20:30:21 +0700

    connected to MongoDB with mongoose

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
-app.get('/', async(req, res) => {
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', async (req, res) => {
   try {
-    console.log('tes');
+    console.log('tes')
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 })
+
+//connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log('connected to DB')
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
