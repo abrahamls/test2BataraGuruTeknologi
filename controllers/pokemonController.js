d634c8190b2a86b4032a773e3b8b4936185bd0a9
abraham linggom siahaan <abraham.siahaan1997@gmail.com>
2022-03-17 23:02:23 +0700

    axios added to fetch data from poke api

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -5,18 +5,20 @@ class PokemonController {
   static async getPokemons(req, res, next) {
     try {
       let offset = 0
-      let limit = 10  
       if (!req.query.page || req.query.page == 0 || req.query.page == 1) {
         offset = 0
       } else {
         offset = req.query.page * 10
       }
-      
+      const result = await axios({
+        method: 'get',
+        url: `${process.env.POKE_API}?offset=${offset}&limit=10`
+      })
+      res.status(result.status).json(result.data.results)
     } catch (error) {
-      console.log(error);   
+      console.log(error)
     }
   }
 }
 
 module.exports = PokemonController
-
