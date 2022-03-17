aa31f4aaf12a1a9cf94aea664e16992f3e9abed8
abraham linggom siahaan <abraham.siahaan1997@gmail.com>
2022-03-17 23:51:53 +0700

    Pokemon API added, will hit poke api and will save the result to mongodb if the poke api hasn been hit before

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -14,9 +14,15 @@ class PokemonController {
         method: 'get',
         url: `${process.env.POKE_API}?offset=${offset}&limit=10`
       })
-      res.status(result.status).json(result.data.results)
+      const samplePokemon = await Pokemon.findOne({ name: result.data.results[0].name }) //to check if the api has already been called before by checking the name of the pokemon 
+      if (samplePokemon) {
+        res.status(200).json({ message: "API ini sudah dihit sebelumnya, dan sudah tersimpan ke database MongoDB" })
+      } else {
+        await Pokemon.insertMany(result.data.results)
+        res.status(201).json({ message: "API ini belum dihit sebelumnya, dan data akan tersimpan ke database MongoDB" })
+      }
     } catch (error) {
-      console.log(error)
+      next(error)
     }
   }
 }
